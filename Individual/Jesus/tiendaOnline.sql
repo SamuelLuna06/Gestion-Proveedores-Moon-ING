57d8fa63ab95483e2e0094e6d83cdede52b35fe6
Jesus1102 <159170739+Jesus1102@users.noreply.github.com>
2024-09-25 08:26:59 -0500

    Update tiendaOnline.sql

--- Individual/Jesus/tiendaOnline.sql
+++ Individual/Jesus/tiendaOnline.sql
@@ -17,7 +17,6 @@ consultar las ordenes generados entrea marzo y junio
 consultar producto que tengan la letra r
 consultar las ventas que tengan productos con precios de 15.000
 
-
 proemdio ventas, sumatoria total, cantidad de clientes, venta mas economica hecha
 */
 
@@ -37,11 +36,12 @@ create table producto_venta(
     idVentaFK int,
     fechaVenta date,
     cantidadProducto int,
+    precioTotal float,
     foreign key (idProductoFK) references Producto(idProducto),
 	foreign key (idVentaFK) references Venta(idVenta)   
 );
 
-create table ventas(
+create table Venta(
 	idVenta int auto_increment primary key,
     numOrden int auto_increment,
     precioVenta float,
@@ -89,3 +89,25 @@ select fechaNUsuario from usuario where rolUsuario like "Empleado";
 select fechaVenta from producto_Venta where fechaVenta between "2024/03/01" and "2024/06/01";
 
 select nombreProducto from Producto where nombreProducto like "r";
+
+
+select sum(cantidadProducto) from producto_venta;
+select avg(precioProducto) from Producto;
+select min(precioTotal) from producto_venta;
+
+/*consultas multitabla*/
+
+/*inner join 
+
+select campos_deseados from tabla1 inner join tabla2 on condicion;
+
+select * from venta inner join cliente on  venta.idClienteFK = Cliente.idCliente;
+ 1. consultar el cliente de la maxima venta hecha
+ 2. consultar usuario y cliente de una venta especifica
+ 3. consultar los productos que compro un cliente especifico 
+ 4. consultar todos los clientes que han hecho compras (tabla ventas)
+ 
+select c.nombreCliente, orden.idClienteFK, dv.precioTotal from orden o inner join cliente c on c.idCliente = o.idClienteFK
+inner join detalleVenta dv on o.
+
+*/
