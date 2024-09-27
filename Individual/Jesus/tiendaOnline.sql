d67310da4300b284b18db986d385b785cc6856e7
Jesus1102 <159170739+Jesus1102@users.noreply.github.com>
2024-09-27 08:49:04 -0500

    Update tiendaOnline.sql

--- Individual/Jesus/tiendaOnline.sql
+++ Individual/Jesus/tiendaOnline.sql
@@ -20,94 +20,108 @@ consultar las ventas que tengan productos con precios de 15.000
 proemdio ventas, sumatoria total, cantidad de clientes, venta mas economica hecha
 */
 
-create database tiendaOnline;
-use tiendaOnline;
+create database Tienda_Online;
+use Tienda_Online;
 
-CREATE TABLE producto (
-    idProducto INT AUTO_INCREMENT,
-    codBarras INT AUTO_INCREMENT PRIMARY KEY,
-    nombreProducto VARCHAR(15),
-    precioProducto FLOAT,
-    cantProducto INT
+create table Producto
+(
+    idProducto int auto_increment primary key,
+    codigoBarrasProducto varchar(5) not null,
+    precioProducto float(10) not null,
+    nombreProducto varchar(20)
 );
 
-create table producto_venta(
-	idProductoFK int,
-    idVentaFK int,
-    fechaVenta date,
-    cantidadProducto int,
-    precioTotal float,
-    foreign key (idProductoFK) references Producto(idProducto),
-	foreign key (idVentaFK) references Venta(idVenta)   
+create table Usuario 
+(
+    idUsuario int auto_increment primary key,
+    nombreUsuario varchar(30) not null,
+    rolUsuario varchar(20) not null
 );
 
-create table Venta(
-	idVenta int auto_increment primary key,
-    numOrden int auto_increment,
-    precioVenta float,
-    idUsuarioFK int,
+create table Cliente
+(
+    idCliente int auto_increment primary key,
+    nombreCliente varchar(30) not null,
+    fechaNacimientoCliente date not null
+);
+
+create table Venta
+(
+    idVenta int auto_increment primary key,
+    numeroVenta int not null,
+    fechaVenta date not null,
     idClienteFK int,
-    
-	foreign key (idUsuarioFK) references usuario(idUsuario),
-    foreign key (idClienteFK) references Cliente(idCliente)
+    idUsuarioFK int,
+    foreign key (idClienteFK) references Cliente(idCliente),
+    foreign key (idUsuarioFK) references Usuario(idUsuario)
 );
 
-create table usuario(
-	idUsuario int primary key,
-	nombreUsuario varchar(20),
-    rolUsuario varchar(15),
-    fechaNUsuario date
-    
+create table Venta_Producto 
+(
+	cantidad int not null,
+    totalVentas int not null,
+    idVentaFK int,
+    idProductoFK int,
+	precioProductoFK float,
+    foreign key (idVentaFK) references Venta(idVenta),
+    foreign key (idProductoFK) references Producto(idProducto),
+    foreign key (precioProductoFK) references Producto(precioProducto)
 );
-
-create table cliente(
-	idCliente int auto_increment primary key,
-    nombreCliente varchar (15)
-);
-
-
-/*Inserts*/
 describe Producto;
-insert into Producto values(" "," ","Shampoo",15000,15);
-insert into Producto values(" "," ","Jabon",8000,25);
-insert into Producto values(" "," ","Gel",7000,16);
-insert into Producto values(" "," ","Acondicionador",11000,18);
+insert into Producto values ("10001", '', 1.000, "Rana"), ("10002", '', 2.000, "Mousepad"), ("10003", '',  15.000, "Arroz"), ("10004", '', 30.000, "Mouse logitech"),("10005",'',8.000,"Gorros Bebe");
+insert into Producto values ("","a1",8000, "Gorros bebe");
+insert into Usuario values ('', "Samuel", "Cliente"), ('', "Jesus", "Empleado"), ('', "Juliana", "Cliente"), ('', "Paula", "Empleado");
+insert into Cliente values ('', "Samuel", "2006-02-10"), ('', "Carlos", "2004-06-22"), ('', "Juliana", "2007-12-01"), ('', "Cristian", "2006-03-19");
+insert into Cliente values ('',"Tatiana","2024-09-27");
 
-describe Ventas;
-
-
-/*Consultas*/
-
+select * from Usuario;
+select * from Cliente;
 select * from Producto;
+select * from Venta;
+select * from Venta_Producto;
 
-select precioProducto from Producto order by precioProducto asc;
-
-select fechaNUsuario from usuario where fechaNUsuario like "%01%";
-
-select fechaNUsuario from usuario where rolUsuario like "Empleado";
-
-select fechaVenta from producto_Venta where fechaVenta between "2024/03/01" and "2024/06/01";
-
-select nombreProducto from Producto where nombreProducto like "r";
+describe venta;
+insert into Venta values ('',1,'2024-09-27',6,6);
 
 
-select sum(cantidadProducto) from producto_venta;
-select avg(precioProducto) from Producto;
-select min(precioTotal) from producto_venta;
+describe Venta_Producto;
 
-/*consultas multitabla*/
+insert into Venta_Producto values (1,1,4,10005,8000);
 
-/*inner join 
+/* modificar*/
 
-select campos_deseados from tabla1 inner join tabla2 on condicion;
+alter table Venta_Producto add numeroVentaFK int not null;
+alter table Venta_Producto add constraint FK_Venta_Orden foreign key (numeroVentaFK) references venta(numeroVenta);
 
-select * from venta inner join cliente on  venta.idClienteFK = Cliente.idCliente;
- 1. consultar el cliente de la maxima venta hecha
- 2. consultar usuario y cliente de una venta especifica
- 3. consultar los productos que compro un cliente especifico 
- 4. consultar todos los clientes que han hecho compras (tabla ventas)
- 
-select c.nombreCliente, orden.idClienteFK, dv.precioTotal from orden o inner join cliente c on c.idCliente = o.idClienteFK
-inner join detalleVenta dv on o.
 
+
+
+
+select* from Producto;
+select codigoBarrasProducto, idProducto from Producto order by precioProducto desc;
+select idUsuario, nombreUsuario from Usuario where rolUsuario = "Empleado";
+select idVenta, numeroVenta from Venta where fechaVenta/*??*/;
+select idProducto, precioProducot from Producto where nombreProducto like '%r%';
+select cantidad, totalVentas from Venta_Producto where precioProductoFK between 15.000 and 30.000;
+
+select avg totalVentas from Venta_Producto;
+select sum cantidad from Venta_Producto;
+select count(idCliente) from Cliente;
+select min(idVentaFK) from Venta_Producto;
+
+/*
+Consultar el cliente que tiene la mayor venta
+Consultar cliente y usuario de una venta especifica
+Consultar los productos que compro un cliente especifico
+Consultar todos los clientes que han hecho ventas
 */
+
+select* from Venta_Producto as VP inner join Cliente as C on C.idCliente where VP.max(precioProductoFK);
+select* from Usuario as U inner join Cliente as C inner join Venta V on U.idUsuario.nombreUsuario and C.idClienteFK.nombreCliente where V.idVenta = 101;
+select* from Cliente as C inner join Producto as P where P.idProducto = C.idCliente;
+select* from Cliente as C inner join Venta as V where C.idCliente = V.idVenta;
+
+
+
+/*update tabla set atributo where condicion*/
+/*update asistencia set asistencia ='r' where id=14;*/
